634246ad80be5699d1de5279ce375ad93b77c0ac
Shizu <evdokimovnikita501@gmail.com>
2024-09-15 20:31:03 +0300

    Create random.js

---
+++ libs/random.js
@@ -0,0 +1,20 @@
+class Random {
+    generate(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() *
+                charactersLength));
+        }
+        return result;
+    }
+    
+    getRandomInt(min, max){
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
+
+module.exports = Random;
